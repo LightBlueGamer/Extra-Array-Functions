a85e7998d58a308ac3aa3ba1951781c33c06e4eb
LightBlueGamer <timjonsson03@hotmail.com>
2021-02-13 21:35:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Extra Array Functions
+==============
+Extra Array Functions is a simple module that adds some utilities for arrays
+___
+## Usage
+* Array.middle() returns the middle of an array;
+* Array.random() returns a random item in the array;
+* Array.toObject() returns a object from the array;
+* Array.toMap() returns a map from the array;
---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = 'arrayutilities'
+
+function tc(item) {
+  if(!Array.isArray(item)) throw Error(`${item} is not of type "Array"`);
+  if(this.length < 1) throw Error(`Arrays length should be atleast 1`)
+};
+
+Array.prototype.random = function() {
+  tc(this);
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+Array.prototype.middle = function() {
+  tc(this);
+  if(this.length <= 1) throw Error(`Array must be of a length more than 1`);
+  else return this[Math.ceil(this.length / 2)];
+};
+
+Array.prototype.toObject = function() {
+  tc(this);
+  const obj = {};
+  for(let i = 0; i<this.length; i++) {
+    obj[`key${i+1}`] = this[i];
+  };
+  return obj;
+};
+
+Array.prototype.toMap = function() {
+  tc(this);
+  return new Map(this.map((v, i) => [i, v]));
+};
